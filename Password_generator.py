184f35dd397e691d8fe0394faecb7b4d31858002
Sanjuchinnu23 <sanjanachinnasamy23@gmail.com>
2025-07-23 22:51:29 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -4,16 +4,12 @@ import string
 def generate_password():
     print("Password Generator")
 
-#Ask for password length
 length = int(input("Enter the desired password length: "))
 
-#Create pool of characters
 all_characters = string.ascii_letters + string.digits + string.punctuation
 
-#Generate password
 password = ''.join(random.choice(all_characters) for i in range(length))
 
-#Show password
 print("Your generated password is:", password)
 
 #Run the function
